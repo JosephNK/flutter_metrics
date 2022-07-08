4a5006774ee1ad5861b01f8651254f19ba92be52
JosephNK <nkw0608@gmail.com>
2022-07-08 14:26:15 +0900

    Fixed MetricsProvider null check

--- lib/src/metrics_extension.dart
+++ lib/src/metrics_extension.dart
@@ -1,8 +1,19 @@
 part of flutter_metrics;
 
 extension BuildContextExtension on BuildContext {
-  Metric get metric => MetricsApp.of(this)!.metric;
+  Metric get metric {
+    final provider = MetricsApp.of(this);
+    if (provider != null) {
+      return provider.metric;
+    }
+    return const Metric(1.0);
+  }
 
-  Future<void> setMetric(Metric metric) =>
-      MetricsApp.of(this)!.setMetric(metric);
+  Future<void> setMetric(Metric metric) async {
+    final provider = MetricsApp.of(this);
+    assert(provider != null, 'MetricsProvider is null');
+    if (provider != null) {
+      provider.setMetric(metric);
+    }
+  }
 }
